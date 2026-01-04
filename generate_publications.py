b4ac250a7e277d1209f980e5bc0d1ac6b096ab63
nkorf <nkorf@users.noreply.github.com>
2026-01-04 23:20:31 +0200

    Use bold formatting for author highlighting

--- generate_publications.py
+++ generate_publications.py
@@ -51,9 +51,9 @@ def format_authors_apa(authors_str, highlight_author="Korfiatis"):
         else:
             author_formatted = last
 
-        # Highlight the specified author with underline
+        # Highlight the specified author with bold
         if highlight_author and highlight_author.lower() in last.lower():
-            author_formatted = f"<ins>{author_formatted}</ins>"
+            author_formatted = f"**{author_formatted}**"
 
         formatted.append(author_formatted)
 
